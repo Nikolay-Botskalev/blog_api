7410a8beca7c357bbf37df62cdd1ede69cec3c71
Nikolay Botskalev <NiNiNikolay1@yandex.ru>
2024-07-21 19:37:26 +0300

    Commit_3

--- blog_api/api/views.py
+++ blog_api/api/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import (
-    IsAuthenticated, IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .permissions import IsAuthorOrReadOnly
 from posts.models import Follow, Group, Post
